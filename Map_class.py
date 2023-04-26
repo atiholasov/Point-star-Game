a337e2fb4430457e3651305901c3884240f4a1b7
Alexey Tiholasov <alexey.tiholasov@apstecsystems.com>
2023-04-26 14:37:34 +0300

    correct work with classes without analyze winer system

--- Map_class.py
+++ Map_class.py
@@ -1,9 +1,13 @@
 import Cell_class as Cell
 
 
+class Out_of_field_coordinates(Exception):
+    pass
+
+
 class Map:
     def __init__(self):
-        self.size = 0
+        self.size = ()
         self.grid = []
 
     def creating_empty_map(self):
@@ -20,3 +24,21 @@ class Map:
         for i in range(self.size[0]):
             for j in range(self.size[1]):
                 self.grid[i].append(Cell.Cell())
+
+    def print_map(self):
+        for i in range(self.size[0]-1, -1, -1):
+            print(i)
+            print(self.grid[i])
+
+    @staticmethod
+    def check_coordinates(coordinates_x, coordinates_y):
+        if int(coordinates_x) <= 0 or int(coordinates_y) <= 0:
+            raise Out_of_field_coordinates("NotLiquid numbers")
+
+    def player_turn(self, coordinates_x, coordinates_y, player_number):
+        write_y_coordinate = int(coordinates_y) - 1
+        write_x_coordinate = int(coordinates_x) - 1
+        if player_number == 0:              # Ходит первый игрок x
+            self.grid[write_y_coordinate][write_x_coordinate].star_choice()
+        else:                               # Ходит второй игрок .
+            self.grid[write_y_coordinate][write_x_coordinate].point_choice()
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import Cell_class as Cell
 import Map_class
 
-move = 0
+
+number_of_move = 0
 PLAYERS_DICT = {0: "X",
                 1: "0"}
 
@@ -10,32 +11,30 @@ Map.creating_empty_map()
 
 while True:
 
-    for i in range(len(Map)):  # Отрисовка карты
-        print(Map[i])
+    Map.print_map()
 
-    now_move = move % 2
-
-    print(f"Игрок {now_move+1} введите координыты {PLAYERS_DICT[now_move]} в формате: x y")
+    player_number = number_of_move % 2
+    print(f"Игрок {player_number+1} введите координыты {PLAYERS_DICT[player_number]} в формате: x y")
 
     try:
         coordinates = str(input())
-        print(coordinates)
         coordinates_x, coordinates_y = coordinates.split(" ")
+        Map.check_coordinates(coordinates_x, coordinates_y)
 
         try:
-            if now_move == 0:                                                                    # Ходит первый игрок x
-                Map[SIZE_OF_MAP[1] - int(coordinates_y)][int(coordinates_x) - 1].star_choice()
-            else:                                                                                # Ходит второй игрок .
-                Map[SIZE_OF_MAP[1] - int(coordinates_y)][int(coordinates_x) - 1].point_choice()
+            Map.player_turn(coordinates_x, coordinates_y, player_number)                       
         except IndexError:
             print("Введенные значения вне игравого поля")
-            move -= 1
+            number_of_move -= 1          
         except Cell.RechoiceError:
             print("Выберите другую точку, данная уже занята")
-            move -= 1
+            number_of_move -= 1
 
     except ValueError:
         print("Следуйте формату ввода: x y")
-        move -= 1
+        number_of_move -= 1
+    except Map_class.Out_of_field_coordinates:
+        print("Вводите положительные координаты")
+        number_of_move -= 1
 
-    move += 1
+    number_of_move += 1
